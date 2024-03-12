83fbb7e72a7fee50944b764085c1ef2b9eb975da
Jerry Nabango <jerrynabango9@gmail.com>
2024-03-12 13:06:36 +0300

    update

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,17 +1,20 @@
-import dbClient from '../utils/db';
 import redisClient from '../utils/redis';
-
+import dbClient from '../utils/db';
 
 class AppController {
-  static getStatus(request, response) {
-    response.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+  static getStatus(req, res) {
+    res.status(200).json({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
 
-  static async getStats(request, response) {
-    const usersNum = await dbClient.nbUsers();
-    const filesNum = await dbClient.nbFiles();
-    response.status(200).json({ users: usersNum, files: filesNum });
+  static async getStats(req, res) {
+    try {
+      const usersCount = await dbClient.nbUsers();
+      const filesCount = await dbClient.nbFiles();
+      res.status(200).json({ users: usersCount, files: filesCount });
+    } catch (err) {
+      res.status(500).json({ error: 'Internal Server Error' });
+    }
   }
 }
 
-module.exports = AppController;
+export default AppController;
